e70648665919ea73f74dcaa567f0673b6b2322ab
Gabriel Cozma <me@gabs.eu.org>
2023-10-16 22:43:05 +0300

    Updated Content, Code Cleanup

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -4,25 +4,25 @@
     <section id="links">
       <div class="grid-container">
         <a class="card" :class="{ locked: !unlocked }" :href="getLink1Url">
-        <div class="head" id="first-head" style="background-color: rgba(255, 71, 71, 0.838);">
+        <div class="head" style="background-color: rgba(255, 141, 71, 0.838);" id="first-head">
+        <h3>ISTORIE</h3>
+        </div>
+        <p class="subject">Napoleon Bonaparte</p>
+        <p class="date">16/10/2023</p>
+      </a>
+        <a class="card" :class="{ locked: !unlocked }" :href="getLink2Url">
+        <div class="head" style="background-color: rgba(255, 71, 71, 0.838);">
         <h3>L. ROMÂNĂ</h3>
         </div>
-        <p class="subject">CP - Casă</p>
+        <p class="subject">CP - Casă & Pic</p>
         <p class="date">13/10/2023</p>
       </a>
-      <a class="card" :class="{ locked: !unlocked }" :href="getLink2Url">
-        <div class="head" style="background-color: rgba(255, 238, 80, 0.538);">
+      <a class="card" :class="{ locked: !unlocked }" :href="getLink3Url">
+        <div class="head" style="background-color: rgba(145, 255, 0, 0.406);">
         <h3>L. ROMÂNĂ</h3>
         </div>
         <p class="subject">Ion Druță</p>
         <p class="date">06/10/2023</p>
-      </a>
-      <a class="card" :class="{ locked: !unlocked }" :href="getLink3Url">
-        <div class="head" style="background-color: rgba(255, 175, 71, 0.838);">
-        <h3>BIO</h3>
-        </div>
-        <p class="subject">GP - Albina</p>
-        <p class="date">16/10/2023</p>
       </a>
       <a class="card">
 
@@ -35,55 +35,55 @@
 </template>
 
 <style scoped>
-  #page {
-    margin: 0 auto;
-    width: 1000px;
-    height: 500px;
-    padding: 40px;
-    background-color: var(--color-background-section);
-    border-radius: 23px;
-    box-shadow: 0px 4px 56px 0px rgba(0, 0, 0, 0.25); 
-  }
+#page {
+  margin: 0 auto;
+  width: 1000px;
+  height: 500px;
+  padding: 40px;
+  background-color: var(--color-background-section);
+  border-radius: 23px;
+  box-shadow: 0px 4px 56px 0px rgba(0, 0, 0, 0.25); 
+}
 
-  #page h1 {
-    font-weight: 900;
-    font-size: 28px;
-  }
+#page h1 {
+  font-weight: 900;
+  font-size: 28px;
+}
   
-  #page #copyright {
-    text-align: right;
-    margin-top: 10%;
-    color: var(--color-text-dim);
-    position: relative;
-  }
+#page #copyright {
+  text-align: right;
+  margin-top: 10%;
+  color: var(--color-text-dim);
+  position: relative;
+}
 
-  .grid-container {
-    margin-top: 80px;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr); /* Two columns */
-    grid-gap: 2px; /* 1px gap between cards */
-    border-radius: 20px;
+.grid-container {
+  margin-top: 80px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 2px;
+  border-radius: 20px;
 }
 
 .card {
-    background-color: var(--color-background-soft); /* Card background color */
-    padding: 20px;
-    text-align: center;
-    height: 200px;
-    transition: background-color 300ms;
+  background-color: var(--color-background-soft);
+  padding: 20px;
+  text-align: center;
+  height: 200px;
+  transition: background-color 300ms;
 }
 
 .card:hover {
   background-color: var(--color-background-mute);
 }
 .card:first-child {
-  border-top-left-radius: 15px; /* Top-left rounded corner for the first card */
-  border-bottom-left-radius: 15px; /* Bottom-left rounded corner for the first card */
+  border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;
 }
 
 .card:last-child {
-  border-top-right-radius: 15px; /* Top-right rounded corner for the last card */
-  border-bottom-right-radius: 15px; /* Bottom-right rounded corner for the last card */
+  border-top-right-radius: 15px; 
+  border-bottom-right-radius: 15px;
 }
 
 .card h3 {
@@ -105,18 +105,18 @@
 .card > .head {
   text-align: center;
   height: 70px;
-text-align: center;
+  text-align: center;
   margin: -20px;
-  align-items: center; /* Center horizontally */
-    justify-content: center; /* Center vertically */
-  }
-
-  #first-head { 
-  border-top-left-radius: 15px; /* Top-left rounded corner for the first card */
+  align-items: center;
+  justify-content: center;
 }
 
-.toggle-password {
-  position: relative;
+#first-head { 
+  border-top-left-radius: 15px;
+}
+
+#last-head {
+  border-top-right-radius: 15px;
 }
 
 .password-input {
